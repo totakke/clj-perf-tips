f2d321c4e564ea0f6a239cdefba367b61c0e835c
Toshiki Takeuchi <roimisia@gmail.com>
2017-12-04 15:52:34 +0900

    Update Libra to 0.1.1

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://qiita.com/totakke/items/e0464da6a269cfc5de6b"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [criterium "0.4.4"]
-                 [net.totakke/libra "0.1.0"]]
-  :plugins [[net.totakke/lein-libra "0.1.0"]]
+                 [net.totakke/libra "0.1.1"]]
+  :plugins [[net.totakke/lein-libra "0.1.1"]]
   :libra {:bench-paths ["src"]})
